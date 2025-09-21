ec46927b14f40e127cc4ce5128f5b03dd822c196
Weslei Schmitt <sschmittweslei@gmail.com>
2025-09-21 20:43:13 -0300

    FEAT: Adicionado novos itens.

--- src/graphic/LoginWindow.java
+++ src/graphic/LoginWindow.java
@@ -4,7 +4,10 @@ import javax.swing.*;
 
 public class LoginWindow extends JFrame
 {
-    
+    private JLabel lblTitulo, lblUsuario, lblSenha;
+    private JTextField txtUsuario;
+    private JPasswordField txtSenha;
+    private JButton btnEntrar;
 
     public LoginWindow()
     {
@@ -13,7 +16,17 @@ public class LoginWindow extends JFrame
         setLocationRelativeTo( null );
         setDefaultCloseOperation( EXIT_ON_CLOSE );
         setLayout( null );
+        componentesCriar();
         setVisible( true );
+    }
+
+    private void componentesCriar()
+    {
+        lblTitulo = new JLabel( "Sistemas Vinhos" );
+        lblTitulo.setBounds( 50, 50, 280, 30 );
+        lblUsuario = new JLabel( "Usuario" );
+        lblSenha = new JLabel( "Senha" );
+        getContentPane().add( lblTitulo );
     }
 
     public static void main( String[] args )

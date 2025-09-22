ec1b1b75314490406d6b8c206bc186b2cff73c03
Weslei Schmitt <sschmittweslei@gmail.com>
2025-09-22 20:45:46 -0300

    Feat: Adicionado novos itens

--- src/graphic/LoginWindow.java
+++ src/graphic/LoginWindow.java
@@ -1,6 +1,7 @@
 package graphic;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class LoginWindow extends JFrame
 {
@@ -23,10 +24,25 @@ public class LoginWindow extends JFrame
     private void componentesCriar()
     {
         lblTitulo = new JLabel( "Sistemas Vinhos" );
-        lblTitulo.setBounds( 50, 50, 280, 30 );
-        lblUsuario = new JLabel( "Usuario" );
-        lblSenha = new JLabel( "Senha" );
+        lblTitulo.setBounds( 30, 20, 280, 30 );
+        lblTitulo.setFont( new Font( Font.MONOSPACED, Font.BOLD, 26 ) );
         getContentPane().add( lblTitulo );
+
+        lblUsuario = new JLabel( "Usuário: " );
+        lblUsuario.setBounds( 20, 70, 100, 30 );
+        getContentPane().add( lblUsuario );
+
+        txtUsuario = new JTextField();
+        txtUsuario.setBounds( 75, 70, 150, 30 );
+        getContentPane().add( txtUsuario );
+
+        lblSenha = new JLabel( "Senha: " );
+        lblSenha.setBounds( 20, 100, 280, 30 );
+        getContentPane().add( lblSenha );
+
+        txtSenha = new JPasswordField();
+        txtSenha.setBounds( 75, 100, 150, 30 );
+        getContentPane().add( txtSenha );
     }
 
     public static void main( String[] args )
